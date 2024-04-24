7e8f74c4f984abc923cff510373dcf7d5f0aa6c2
JaQwae <105896967+JaQwae@users.noreply.github.com>
2024-04-23 22:00:34 -0700

    loading screen created

--- src/components/loading-screen/LoadingScreen.jsx
+++ src/components/loading-screen/LoadingScreen.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./LoadingScreen.css"
 
 const LoadingScreen = () => {
+  // return (
+  //   <div>LoadingScreen</div>
+  // )
+
   return (
-    <div>LoadingScreen</div>
-  )
+    <section id="loading-container">
+      <div className="water"></div>
+      <h3>Loading</h3>
+    </section>
+)
 }
 
 export default LoadingScreen
